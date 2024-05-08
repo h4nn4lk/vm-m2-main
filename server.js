1254038c849f1c0d0983737dfc18e151d4a0ad60
Virgo Egert Vahesaar <egertvahesaar90@gmail.com>
2024-05-08 15:42:50 +0300

    hullumaja

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const nunjucks = require('nunjucks')
 const app = express();
 const port = 3000;
+const cookieParser = require('cookie-parser');
+const { Movie, User } = require('./models/index.js');
 
-const cookieParser = require('cookie-parser')
 app.use(cookieParser())
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
@@ -16,58 +17,85 @@ app.use(session({
 
 
 app.use(express.urlencoded({
-  extended:true
+  extended: true
 }));
 
 
 const env = nunjucks.configure('views', {
-    autoescape: true,
-    express: app
+  autoescape: true,
+  express: app
 });
 
 app.use((req, res, next) => {
   env.addGlobal('user', req.session.user);
   env.addGlobal('errors', req.session.errors);
-  req.session.errors =null;
+  req.session.errors = null;
   req.session.save();
   next();
 });
 
-app.get('/', (req, res) => {
-  res.render('index.njk');
+app.get('/', async (req, res) => {
+  let pageCount = 16;
+  let page = parseInt(req.query.page ?? 1);
+  let offset = (page - 1) * pageCount
   //console.log('somebody visited');
+  const movies = await Movie.findAll({ limit: 16, offset: offset });
+  let count = await Movie.count();
+  let pages = Math.ceil(count / pageCount);
+  let elements = [];
+  for (let i = 1; i <= 3; i++) {
+    
+    elements[i] = i;
+  }
+  if (page > 1) {
+    elements.push('...');
+  }
+  for (let i = page - 2; i <= page + 2 && i < pages && i > 0; i++) {
+   
+    elements[i] = i;
+  }
+  if (page < pages - 3) {
+    elements.push('...');
+  }
+  for (let i = pages - 2; i < pages; i++) {
+    elements[i] = i;
+  }
+
+  elements = elements.filter(e => e);
+ 
+  res.render('index.njk', { movies, elements, page, pages });
 });
 
 app.get('/page2', (req, res) => {
-    res.render ('page2.njk');
+  res.render('page2.njk');
 });
 
 app.get('/form', (req, res) => {
   console.log(req.query);
-  res.render ('form.njk', req.query);
+  res.render('form.njk', req.query);
 });
 
 
 app.get('/circle', (req, res) => {
   console.log(req.query);
-  res.render ('circle.njk', req.query);
+  res.render('circle.njk', req.query);
 });
 
 app.post('/circle', (req, res) => {
   let area = Math.PI * req.body.radius * req.body.radius;
   let diameter = 2 * Math.PI * req.body.radius;
-  let volume = 4/3 * Math.PI * req.body.radius * req.body.radius * req.body.radius;
-  res.render('circleAnswer.njk',{r: req.body.radius, a: area, d: diameter, v: volume });
+  let volume = 4 / 3 * Math.PI * req.body.radius * req.body.radius * req.body.radius;
+  res.render('circleAnswer.njk', { r: req.body.radius, a: area, d: diameter, v: volume });
 });
-const movieController =require('./src/movieController.js');
+const movieController = require('./src/movieController.js');
 app.use('/movies', movieController);
 
-app.get('/cookie', (req, res)=> {
-res.cookie('mycookie', 'sexy cookie', {maxAge:1000*60*60*24*30})
-res.send(req.cookies);
-if(!req.session.secretValue)
-res.session.secretValue = 'shush baby'
-res.send(req.session);
+app.get('/cookie', (req, res) => {
+  res.cookie('mycookie', 'sexy cookie', { maxAge: 1000 * 60 * 60 * 24 * 30 })
+  res.send(req.cookies);
+  if (!req.session.secretValue)
+    res.session.secretValue = 'shush baby'
+  res.send(req.session);
 });
 const authController = require('./src/authController.js');
 app.use(authController)
